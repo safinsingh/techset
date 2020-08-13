4d7982c30d160d003a8c47f8f8fa7b1cd761f0f9
safinsingh <safin.singh@gmail.com>
2020-08-13 00:49:35 -0700

    Patch builds

--- scripts/build.js
+++ scripts/build.js
@@ -1,39 +1,62 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs')
+const path = require('path')
+const chalk = require('chalk')
 
-const templatePath = path.join(__dirname, "..", "themes", "template.json");
-const variantPath = path.join(__dirname, "..", "themes", "variants.json");
-const distPath = path.join(__dirname, "..", "dist");
+const log = console.log
+const { green } = chalk
 
-const variants = JSON.parse(fs.readFileSync(variantPath));
-const template = JSON.parse(fs.readFileSync(templatePath));
+const templatePath = path.join(__dirname, '..', 'themes', 'template.json')
+const variantPath = path.join(__dirname, '..', 'themes', 'variants.json')
+const packagePath = path.join(__dirname, '..', 'package.json')
+const distPath = path.join(__dirname, '..', 'dist')
+
+const variants = JSON.parse(fs.readFileSync(variantPath))
+const template = JSON.parse(fs.readFileSync(templatePath))
+const package = JSON.parse(fs.readFileSync(packagePath))
+
+log(green('[+] Successfully read variants and template!'))
 
 const toOfficial = (str) => {
-  const arr = str.split("-");
-  const res = arr.forEach((s) => {
-    return `${s[0].toUpperCase() + s.slice(1)} `;
-  });
-  return res;
-};
+  const arr = str.split('-')
+  let res = ''
+  arr.forEach((s) => {
+    res += `${s[0].toUpperCase() + s.slice(1)} `
+  })
+  return res.slice(0, -1)
+}
+
+package.contributes.themes = []
 
 Object.keys(variants).forEach((theme) => {
-  let newFile = template;
-  newFile.name = toOfficial(theme);
+  let newFile = JSON.parse(JSON.stringify(template))
+  newFile.name = toOfficial(theme)
+  package.contributes.themes.push({
+    label: toOfficial(theme),
+    uiTheme: 'vs-dark',
+    path: `./dist/${theme}.json`,
+  })
 
-  Object.keys(variants[theme].changes).forEach((change) => {
-    newFile.colors[change] = variants[theme].changes[change];
-  });
+  Object.keys(variants[theme].colors).forEach((change) => {
+    newFile.colors[change] = variants[theme].colors[change]
+  })
 
-  Object.keys(variants[theme].tokenColors).forEach((el) => {
-    variants[theme].tokenColors.forEach((themeEl) => {
-      if (el.scope === themeEl.scope) {
-        el = themeEl;
+  variants[theme].tokenColors.forEach((token) => {
+    newFile.tokenColors.forEach((themeEl, index) => {
+      if (token.scope === themeEl.scope) {
+        newFile.tokenColors[index] = token
       }
-    });
-  });
+    })
+  })
 
-  fs.writeFileSync(
-    path.join(distPath, `${theme}-test.json`),
-    JSON.stringify(newFile)
-  );
-});
+  fs.writeFile(
+    path.join(distPath, `${theme}.json`),
+    JSON.stringify(newFile, null, 2),
+    () => {
+      log(green(`[+] Successfully generated ${toOfficial(theme)} Theme!`))
+    }
+  )
+})
+
+fs.writeFile(packagePath, JSON.stringify(package, null, 2), () => {
+  log(green(`[+] Successfully generated new package.json!`))
+})
